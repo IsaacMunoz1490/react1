5215c1de78b5118ff111db929154cd8837f96b0d
Isaac A Munoz <isaacmunoz1490@gmail.com>
2021-06-25 18:57:27 -0700

    Breadcrumbs

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
-function RenderDirectoryItem({campsite}) {
+function RenderDirectoryItem({ campsite }) {
     return (
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -27,7 +30,17 @@ function Directory(props) {
     return (
         <div className="container">
             <div className="row">
-                {directory}
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+                <div className="row">
+                    {directory}
+                </div>
             </div>
         </div>
     );
